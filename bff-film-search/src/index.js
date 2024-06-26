5530402d6d7f81c5077135625b1bd8446f6e361e
simolex <simolex@yandex.ru>
2024-06-26 16:08:58 +0300

    save

--- bff-film-search/src/index.js
+++ bff-film-search/src/index.js
@@ -1,13 +1,77 @@
 import { createServer } from "node:http";
+import process from "node:process";
 import { parse } from "node:url";
+
+import { StringDecoder } from "node:string_decoder";
+import { resolve } from "node:path";
+import fs from "node:fs";
 // import path from "node:path";
+
+const envPort = process.env.PORT ? process.env.PORT : 3000;
+const envBackupPath = process.env.BACKUP_FILE_PATH
+    ? process.env.BACKUP_FILE_PATH
+    : resolve("backup", "db_backup.txt");
+
+const readImage = (pathFile, res) => {
+    const content = fs.readFile(pathFile, function (err, content) {
+        if (err) {
+            const payload = {
+                message: "File Not Found",
+                code: 404,
+            };
+            res.setHeader("Content-Type", "application/json");
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.writeHead(404);
+            res.write(JSON.stringify(payload));
+            res.end("\n");
+        } else {
+            res.writeHead(200, { "Content-Type": "image/jpg" });
+            res.end(content);
+        }
+    });
+};
 
 const routes = {
     ping: function (data, res) {
         res.write(JSON.stringify({ pong: true }));
         res.end();
     },
-    notFound: function (data, res) {},
+    echo: function (data, res) {
+        switch (data.method) {
+            case "POST":
+                res.write(JSON.stringify(data.buffer));
+                break;
+            case "GET":
+                res.writeHead(200);
+                break;
+        }
+
+        res.end();
+    },
+    img: function (data, res) {
+        readImage(resolve("src", "logo.jpeg"), res);
+    },
+    notFound: function (data, res) {
+        const payload = {
+            message: "File Not Found",
+            code: 404,
+        };
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.writeHead(404);
+        res.write(JSON.stringify(payload));
+        res.end("\n");
+    },
+};
+
+const isFileAccess = (pathFile) => {
+    fs.access(pathFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log("no exists");
+            return;
+        }
+        console.log("exists");
+    });
 };
 
 const server = createServer((req, res) => {
@@ -17,24 +81,29 @@ const server = createServer((req, res) => {
     path = path.replace(/^\/+|\/+$/g, "");
 
     console.log(path);
+    isFileAccess(resolve("src", "logo.jpeg"));
 
     let queryString = parsedURL.query;
     let headers = req.headers;
     let method = req.method.toLocaleUpperCase();
+    let decoder = new StringDecoder("utf-8");
+    let buffer = "";
 
-    req.on("data", function () {
-        console.log("recived data");
+    req.on("data", function (chunk) {
+        buffer += decoder.write(chunk);
     });
     req.on("end", function () {
-        let route = routes[path] !== undefined ? routes[path] : routes["notFound"];
+        buffer += decoder.end();
+        let route = routes[path] !== undefined ? routes[path] : routes.notFound;
         let data = {
             path,
             queryString,
             headers,
             method,
+            buffer,
         };
         route(data, res);
     });
 });
 
-server.listen(3000);
+server.listen(envPort);
